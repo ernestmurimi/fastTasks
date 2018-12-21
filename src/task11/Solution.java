842dcbd574b47e46393ee9457b5cb144ebe01edd
Michal Sroka <misroka95@gmail.com>
2018-12-21 15:58:27 +0100

    Second, recursive solution

--- src/task11/Solution.java
+++ src/task11/Solution.java
@@ -6,14 +6,17 @@ public class Solution {
 	public static void main(String[] args) {
 		Solution obj = new Solution();
 		int[] arr = {3, 8, 9, 7, 6};
-		System.out.println(Arrays.toString(obj.solution(arr, 3)));
+		int rotations = 3;
+		System.out.println("Solution #1: " + Arrays.toString(obj.solution1(arr, rotations)));
+		System.out.println("Solution #2: " + Arrays.toString(obj.solution2(arr, rotations)));
 	}
 
 	public Solution() {
 	}
 
-	public int[] solution(int[] A, int K) {
-		if (A.length == 0 || A.length == K || isArrayUniform(A)) {
+	// A iterative solution - faster but with worst space complexity
+	public int[] solution1(int[] A, int K) {
+		if (A.length == 0 || K % A.length == 0 || isArrayUniform(A)) {
 			return A;
 		}
 		int[] B = new int[A.length];
@@ -21,6 +24,23 @@ public class Solution {
 			B[(i + K) % A.length] = A[i];
 		}
 		return B;
+	}
+
+	// A recursive solution - slower but "in-place"
+	public int[] solution2(int[] A, int K) {
+		if (A.length == 0 || K % A.length == 0 || isArrayUniform(A)) {
+			return A;
+		}
+		moveItemToNewLocation(A, K, 0);
+		return A;
+	}
+
+	private void moveItemToNewLocation(int[] A, int K, int idx) {
+		int tmp = A[idx];
+		if (idx + 1 < A.length) {
+			moveItemToNewLocation(A, K, idx + 1);
+		}
+		A[(idx + K) % A.length] = tmp;
 	}
 
 	private boolean isArrayUniform(int[] arr) {

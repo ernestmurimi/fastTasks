382b5d3b9deb475c306ecf78e983401dc5b3aed3
Michal Sroka <misroka95@gmail.com>
2019-01-31 22:19:01 +0100

    [TASK15] Whole task done :)

---
+++ src/task15/Solution.java
@@ -0,0 +1,33 @@
+package task15;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static void main(String[] args) {
+		Solution obj = new Solution();
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 1000, 2000, 5000, 5001, 5002, 5003, 5004, 5005, 5006, 5007, 5008, 5009, 5010, 5011, 5012, 5013, 5014, 5015, 5016, 5017, 5018, 5019, 5020, 5021, 5022, 5023, 5024, 5025, 5026, 5027, 5028, 5029, 5030, 5031, 5032, 5033, 5034, 5035, 5036, 5037, 5038, 5039, 5040, 5041, 5042, 5043, 5044, 5045, 5046, 5047, 5048, 5049, 5050, 5051, 5052, 5053, 5054, 5055, 5056, 5057, 5058, 5059, 5060, 5061, 5062, 5063, 5064, 5065, 5066, 5067, 5068, 5069, 5070, 5071, 5072, 5073, 5074, 5075, 5076, 5077, 5078, 5079, 5080, 5081, 5082, 5083, 5084, 5085, 5086, 5087, 5088, 5089, 5090, 5091, 5092, 5093, 5094, 5095, 5096, 5097, 5098, 5099};
+		int number = 3000;
+		try {
+			System.out.println(Arrays.toString(obj.solution(array, number)));
+		} catch (ThereIsNoProperPair e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public int[] solution(int[] ascArray, int sumToSearch) throws ThereIsNoProperPair {
+		int idx1 = 0;
+		int idx2 = ascArray.length - 1;
+		int currSum;
+		while (idx1 < idx2) {
+			currSum = ascArray[idx1] + ascArray[idx2];
+			if (currSum < sumToSearch) {
+				idx1++;
+			} else if (currSum > sumToSearch) {
+				idx2--;
+			} else {
+				return new int[]{ascArray[idx1], ascArray[idx2]};
+			}
+		}
+		throw new ThereIsNoProperPair("There is no proper pair!");
+	}
+}
---
+++ src/task15/ThereIsNoProperPair.java
@@ -0,0 +1,7 @@
+package task15;
+
+class ThereIsNoProperPair extends Exception {
+	ThereIsNoProperPair(String message) {
+		super(message);
+	}
+}

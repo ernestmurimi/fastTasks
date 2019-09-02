30a6a3fd44263517b8d5aceb87f64ebd86669125
Michal Sroka <misroka95@gmail.com>
2019-09-02 18:09:58 +0200

    Task7 refactoring

--- src/task7/Solution.java
+++ src/task7/Solution.java
@@ -9,21 +9,30 @@ public class Solution {
 	public Solution() {
 	}
 
-	public String solution(String toCompress) {
-		StringBuilder compressedTxtAsBuilder = new StringBuilder();
-		char currentChar = '\0';
+	public String solution(String stringToCompress) {
+		StringBuilder compressedText = new StringBuilder();
+		int index = 0;
 		int counter = 0;
-		for (int i = 0; i < toCompress.length(); i++) {
-			if (toCompress.charAt(i) != currentChar) {
-				currentChar = toCompress.charAt(i);
-				counter = 1;
-			} else {
-				counter++;
+		for (char character : stringToCompress.toCharArray()) {
+			counter++;
+			if (isLastSameChar(stringToCompress, index)) {
+				compressedText.append(counter).append(character);
+				counter = 0;
 			}
-			if (i + 1 >= toCompress.length() || toCompress.charAt(i + 1) != currentChar) {
-				compressedTxtAsBuilder.append(Integer.toString(counter)).append(currentChar);
-			}
+			index++;
 		}
-		return compressedTxtAsBuilder.toString();
+		return compressedText.toString();
+	}
+
+	private boolean isLastSameChar(String string, int charIndex) {
+		return isLastChar(string, charIndex) || !isNextCharSame(string, charIndex);
+	}
+
+	private boolean isLastChar(String string, int charIndex) {
+		return charIndex + 1 == string.length();
+	}
+
+	private boolean isNextCharSame(String string, int charIndex) {
+		return string.charAt(charIndex + 1) == string.charAt(charIndex);
 	}
 }
